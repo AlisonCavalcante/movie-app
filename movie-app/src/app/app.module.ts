cd524c144208638cb0e54c6426e7e6b830964fdc
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-07-13 22:03:52 -0300

    create initial card e list movies

--- movie-app/src/app/app.module.ts
+++ movie-app/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CardFilmeComponent } from './components/card-filme/card-filme.component';
+import { ListFilmesComponent } from './components/list-filmes/list-filmes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    CardFilmeComponent,
+    ListFilmesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ movie-app/src/app/shared/Constantes.ts
@@ -0,0 +1,3 @@
+export class Constantes {
+  public static URL_BASE = 'https://api.themoviedb.org/3/movie/';
+}
