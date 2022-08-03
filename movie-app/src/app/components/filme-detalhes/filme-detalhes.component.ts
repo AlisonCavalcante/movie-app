968902e4f9145cad916dbc5f23fd428ec4dd7ce4
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-08-03 17:44:56 -0300

    create get  elenco

--- movie-app/src/app/components/filme-detalhes/filme-detalhes.component.ts
+++ movie-app/src/app/components/filme-detalhes/filme-detalhes.component.ts
@@ -1,3 +1,4 @@
+import { Elenco } from './../../shared/models/Elenco';
 import { MovieService } from './../../services/movie.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -11,18 +12,27 @@ export class FilmeDetalhesComponent implements OnInit {
 
   idFilme!: number;
   movie: any;
+  elenco!: Elenco[];
+
   constructor(private activeRouter: ActivatedRoute, private movieService: MovieService) { }
 
   ngOnInit(): void {
    this.idFilme = this.activeRouter.snapshot.params['id'];
    this.getMovie(this.idFilme);
+   this.getElenco(this.idFilme);
   }
 
   getMovie(id: number){
     this.movieService.getMovie(id).subscribe( res=> {
       this.movie = res;
-      console.log(this.movie)
      });
+  }
+
+  getElenco(id: number) {
+    this.movieService.getCredits(id).subscribe(res => {
+      this.elenco = res.cast;
+      console.log(this.elenco);
+    });
   }
 
 }
--- movie-app/src/app/services/movie.service.ts
+++ movie-app/src/app/services/movie.service.ts
@@ -5,23 +5,33 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getUpcomingMovies(page: number): Observable<any>{
-    return this.http.get<any>(Constantes.URL_BASE + 'upcoming?api_key=' + Constantes.API_KEY + `&page=${page}`);
+  getUpcomingMovies(page: number): Observable<any> {
+    return this.http.get<any>(
+      Constantes.URL_BASE +
+        'upcoming?api_key=' +
+        Constantes.API_KEY +
+        `&page=${page}`
+    );
   }
 
-  getMovie(id: number): Observable<any>{
-    return this.http.get<any>(Constantes.URL_BASE + `${id}?api_key=${Constantes.API_KEY}&language=en-US`);
+  getMovie(id: number): Observable<any> {
+    return this.http.get<any>(
+      Constantes.URL_BASE + `${id}?api_key=${Constantes.API_KEY}&language=en-US`
+    );
   }
 
-  getGenres(): Observable<any>{
-    return this.http.get<any>(Constantes.URL_GENRES + `${Constantes.API_KEY}`)
+  getGenres(): Observable<any> {
+    return this.http.get<any>(Constantes.URL_GENRES + `${Constantes.API_KEY}`);
   }
 
-
+  getCredits(id: number): Observable<any> {
+    return this.http.get<any>(
+      Constantes.URL_BASE + `${id}/credits?api_key=${Constantes.API_KEY}&language=en-US`
+    );
+  }
 }
